e827aea4c9159dbd91ca4fe357495f051dae4e44
nabilakharisma <nabilakharisma1803@gmail.com>
2023-06-16 13:06:33 +0800

    Project 1 dan 2

---
+++ H071221086_TUGAS_PROYEK/src/controller/DbController.java
@@ -0,0 +1,123 @@
+package controller;
+
+import java.sql.SQLException;
+
+import config.MyConfig;
+import model.Produk;
+
+public class DbController extends MyConfig {
+
+    public static void getDatabase() {
+        connection();
+        try {
+            // query = "SELECT nama, harga, stok FROM tb_table ORDER BY ID DESC";
+            query = "SELECT NAMA, HARGA, STOK FROM tb_table";
+
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()) {
+                System.out.println(
+                    String.format("%s - Rp.%d - Stok %d", resultSet.getString("nama"), resultSet.getInt("harga"), resultSet.getInt("stok"))
+                );
+            }
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Produk getProdukByNama(String nama) {
+        Produk produk = null;
+        connection();
+        query = "SELECT * FROM tb_table WHERE NAMA=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, nama);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                produk = new Produk(resultSet.getInt("id"), resultSet.getString("nama"), resultSet.getLong("harga"), resultSet.getInt("stok"));
+            }
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return produk;
+    }
+
+    public static boolean insertData(String nama, long harga, int jumlah) {
+        connection();
+        query = "INSERT INTO tb_table (NAMA, HARGA, STOK) VALUES (?, ?, ?)";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, nama);
+            preparedStatement.setLong(2, harga);
+            preparedStatement.setInt(3, jumlah);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void updateNama(int id, String nama) {
+        connection();
+        query = "UPDATE tb_table SET NAMA=? WHERE id=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, nama);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateHarga(int id, long harga) {
+        connection();
+        query = "UPDATE tb_table SET HARGA=? WHERE id=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setLong(1, harga);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateJumlah(int id, int jumlah) {
+        connection();
+        query = "UPDATE tb_table SET STOK=? WHERE id=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, jumlah);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean deleteData(String nama) {
+        connection();
+        query = "DELETE FROM tb_table WHERE NAMA=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, nama);
+            int affectedRowDelete = preparedStatement.executeUpdate();
+            if (affectedRowDelete > 0) {
+                return true;
+            }
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
